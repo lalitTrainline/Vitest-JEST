032baf8b1727a2c6cc7217b52699922ff14953f8
lalitTrainline <lalit.kumar@thetrainline.com>
2023-09-05 10:44:48 +0530

    more tests

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -28,3 +28,27 @@ it("should yield correct sum if array of numeric string value is provided", () =
     const expectedResult = arrNumbers.reduce((prevValue, curValue) => +prevValue + +curValue , 0);
     expect(result).toBe(expectedResult);
 })
+
+it("should yield 0 if an empty is provided", () => {
+    const arrNumbers = [];
+    const result = add(arrNumbers);
+    expect(result).toBe(0);
+})
+
+it("should throw an error if no value is passed to the function", () => {
+    const resultFn = () => {
+        add();
+    };
+    expect(resultFn).toThrow();
+})
+
+it("should throw an error if multiple argument is provided", () => {
+    const num1 = 1;
+    const num2 = 2;
+
+    //const result = add(num1, num2);
+    const resultFn = () => {
+        add(num1, num2);
+    }
+    expect(resultFn).toThrow(/is not iterable/);
+})
